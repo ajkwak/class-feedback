8a68cd5533e122f8ca630c21cae49b2b4bb4ebc5
ajkwak <ajkwak@users.noreply.github.com>
2014-02-28 20:32:07 -0800

    Upgrade the database to only allow 1 comment per person

--- ClassFeedback/src/edu/mills/cs180a/classfeedback/MySQLiteOpenHelper.java
+++ ClassFeedback/src/edu/mills/cs180a/classfeedback/MySQLiteOpenHelper.java
@@ -1,59 +1,62 @@
 package edu.mills.cs180a.classfeedback;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Creates and upgrades a database for storing {@link Comment}s.
  * Clients should access the database through {@link CommentsDataSource}.
- * 
+ *
  * @author ellen.spertus@gmail.com (Ellen Spertus)
  */
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "comments.db";
-    private static final int DATABASE_VERSION = 1;
-    
+    private static final int DATABASE_VERSION = 2;
+
     /**
      * The name of the table storing comments.
      */
     static final String TABLE_COMMENTS = "comments";
-    
+
     /**
      * The name of the column containing a unique id.
      */
     static final String COLUMN_ID = "_id";
-    
+
     /**
      * The name of the column containing the index of the recipient in
      * {@link Person#everyone}.
      */
     static final String COLUMN_RECIPIENT = "recipient";
-    
+
     /**
      * The name of the column containing the comment's content.
      */
     static final String COLUMN_CONTENT = "content";
-    
+
     /**
      * The index of the column containing a unique id.
      */
     static final int COLUMN_ID_POS = 0;
-    
+
     /**
      * The index of the column containing the comment recipient.
      */
     static final int COLUMN_RECIPIENT_POS = 1;
-    
+
     /**
      * The index of the column containing the comment's content.
      */
     static final int COLUMN_CONTENT_POS = 2;
-    
+
     private static final String DATABASE_CREATE = "create table "
             + TABLE_COMMENTS + "("
             + COLUMN_ID  + " integer primary key autoincrement, "
-            + COLUMN_RECIPIENT + " text not null, "
+            + COLUMN_RECIPIENT + " text unique not null, "
             + COLUMN_CONTENT + " text not null);";
 
     public MySQLiteOpenHelper(Context context) {
@@ -65,8 +68,45 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
         db.execSQL(DATABASE_CREATE);
     }
 
+    private static final String TAG = "MySQLiteOpenHelper";
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        throw new UnsupportedOperationException("There is only one version.");
+        Log.d(TAG, "upgrading database from version " + oldVersion + " to version " + newVersion);
+        String tmpCommentsTable = TABLE_COMMENTS + "_tmp";
+        Log.d(TAG, "temp comments table name is: " + tmpCommentsTable);
+        // Rename the old comments table.
+        String sql = "alter table " + TABLE_COMMENTS + " rename to " + tmpCommentsTable + ";";
+        Log.d(TAG, "Renamed old comments table: " + sql);
+        db.execSQL(sql);
+
+        // Create the new comments table.
+        onCreate(db);
+        Log.d(TAG, "Created the new comments table: " + DATABASE_CREATE);
+
+        // Populate the new comments table with the contents of the old comments table. If there are
+        // multiple comments associated with an e-mail, concatenate them, separating the comments
+        // with a single space.
+        Cursor cursor = db.query(tmpCommentsTable, new String[] {
+                COLUMN_RECIPIENT,
+                "Group_Concat(" + COLUMN_CONTENT + ", ' ')" }, null, null,
+                COLUMN_RECIPIENT, null, null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            ContentValues values = new ContentValues();
+            String recipient = cursor.getString(0);
+            String content = cursor.getString(1);
+            values.put(COLUMN_RECIPIENT, recipient);
+            values.put(COLUMN_CONTENT, content);
+            long insertId = db.insert(MySQLiteOpenHelper.TABLE_COMMENTS, null, values);
+            Log.d(TAG, "Inserted comment " + insertId + " to " + recipient + ": "
+                    + content);
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+        sql = "DROP TABLE " + tmpCommentsTable + ";";
+        Log.d(TAG, "delete old (temp) comments table: " + sql);
+        db.execSQL(sql);
     }
 }
